005b40a8fe3de2d476e95e1257f715a5b8890927
Bruce Snyder <bruce.snyder@gmail.com>
2015-12-15 21:49:36 -0700

    Initial import

---
+++ src/main/java/org/bsnyder/spring/rest/config/AppConfig.java
@@ -0,0 +1,9 @@
+package org.bsnyder.spring.rest.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "org.bsnyder.spring")
+public class AppConfig {
+}
